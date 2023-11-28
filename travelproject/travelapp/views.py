cdaa7f5ad5faa1c9a750a1d3f991826fc1c9de0f
sonybiji <sonysb20121995@gmail.com>
2023-11-28 21:18:11 +0530

    done

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Place
+from .models import Team
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1 = Team.objects.all()
+    return render(request, "index.html",{'result': obj, 'result1': obj1})
